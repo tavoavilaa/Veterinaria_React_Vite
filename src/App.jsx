c0a6eeb0bdb86e807fd6294f65a7e488afd0e7c4
Gustavo Avila <info.gustavoavila@gmail.com>
2022-05-01 00:40:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import {Header} from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+import Formulario from "./components/Formulario";
+import {useState, useEffect} from "react";
+
+function App() {
+
+    const [pacientes, setPacientes] = useState([]);
+    const [paciente, setPaciente] = useState({});
+
+
+    useEffect(() => {
+        const obtenerLS = () => {
+            const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+            setPacientes(pacientesLS)
+
+            console.log(pacientesLS)
+        }
+        obtenerLS();
+    },[]);
+
+    useEffect(() => {
+        localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+    }, [pacientes])
+
+    const eliminarPaciente = id => {
+        const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+
+        setPacientes(pacientesActualizados)
+    }
+
+  return (
+    <div className="container mx-auto mt-10">
+      <Header />
+
+      <div className="mt-12 md:flex">
+          <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+          />
+          <ListadoPacientes
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+
+export const Error = ({mensaje}) => {
+    return (
+        <div
+            className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>{mensaje}</p>
+        </div>
+    );
+};
